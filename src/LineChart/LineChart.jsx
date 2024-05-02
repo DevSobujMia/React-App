513025c32864bb6230c3fc7772a5aeb750a3c982
DevSobujMia <devsobujmia@gmail.com>
2024-05-03 03:05:09 +0600

    add PieChart

--- src/LineChart/LineChart.jsx
+++ src/LineChart/LineChart.jsx
@@ -1,4 +1,4 @@
-import { LineChart as LChart, Legend, Line, Tooltip, XAxis, YAxis } from 'recharts';
+import { LineChart as LChart, Legend, Line, Pie, PieChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const LineChart = () => {
     const subjectMarksData = [
@@ -14,18 +14,45 @@ const LineChart = () => {
         { id: 10, name: 'Jack', math: 82, physics: 66, chemistry: 98 }
     ];
 
+    // Prepare data for PieChart
+    const data01 = subjectMarksData.map(student => ({ name: student.name, value: student.math }));
+    const data02 = subjectMarksData.map(student => ({ name: student.name, value: student.physics }));
+
     return (
-        <div>
-            <LChart width={800} height={400} data={subjectMarksData}>
-                <XAxis dataKey="name" />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                <Line type="monotone" dataKey="math" stroke="#8884d8" />
-                <Line type="monotone" dataKey="physics" stroke="#82ca9d" />
-                <Line type="monotone" dataKey="chemistry" stroke="#ffc658" />
-            </LChart>
-        </div>
+      <div className='flex justify-evenly my-32'>
+        <LChart width={800} height={400} data={subjectMarksData}>
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="math" stroke="#8884d8" />
+          <Line type="monotone" dataKey="physics" stroke="#82ca9d" />
+          <Line type="monotone" dataKey="chemistry" stroke="#ffc658" />
+        </LChart>
+
+        <PieChart className='mt-4' width={730} height={250}>
+          <Pie
+            data={data01}
+            dataKey="value"
+            nameKey="name"
+            cx="50%"
+            cy="50%"
+            outerRadius={50}
+            fill="#8884d8"
+          />
+          <Pie
+            data={data02}
+            dataKey="value"
+            nameKey="name"
+            cx="50%"
+            cy="50%"
+            innerRadius={60}
+            outerRadius={80}
+            fill="#82ca9d"
+            label
+          />
+        </PieChart>
+      </div>
     );
 };
 
